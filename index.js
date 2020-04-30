d7eac20309653b25316b568c85358975caf9627a
Victor <victor@gorchilov.com>
2020-04-30 10:14:15 +0300

    [update] separate folder for commands

--- index.js
+++ index.js
@@ -22,113 +22,23 @@ client.on("message", async (message) => {
     if (message.author.bot || !message.content.startsWith(prefix)) return
 
     const serverQueue = queue.get(message.guild.id)
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
 
-    if (message.content.startsWith(`${prefix}play`)) {
+    if (command == "play") {
         execute(message, serverQueue)
         return
-    } else if (message.content.startsWith(`${prefix}skip`)) {
+    } else if (command == "skip") {
         skip(message, serverQueue)
         return
-    } else if (message.content.startsWith(`${prefix}stop`)) {
+    } else if (command == "stop") {
         stop(message, serverQueue)
         return
-    } else if (message.content.startsWith(`${prefix}queue`)) {
+    } else if (command == "queue") {
         queue(message, serverQueue)
-    } else if (message.content.startsWith(`${prefix}help`)) {
-        help(message, serverQueue)
-    } else {
-        message.channel.send("You need to enter a valid command!")
+    } else if (command == "help") {
+        help(message)
     }
 })
-
-async function execute(message, serverQueue) {
-    const args = message.content.split(" ")
-
-    const voiceChannel = message.member.voice.channel
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        )
-    const permissions = voiceChannel.permissionsFor(message.client.user)
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        )
-    }
-
-    const songInfo = await ytdl.getInfo(args[1])
-    const song = {
-        title: songInfo.title,
-        url: songInfo.video_url,
-    }
-
-    if (!serverQueue) {
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true,
-        }
-
-        queue.set(message.guild.id, queueContruct)
-
-        queueContruct.songs.push(song)
-
-        try {
-            let connection = await voiceChannel.join()
-            queueContruct.connection = connection
-            play(message.guild, queueContruct.songs[0])
-        } catch (err) {
-            console.log(err)
-            queue.delete(message.guild.id)
-            return message.channel.send(err)
-        }
-    } else {
-        serverQueue.songs.push(song)
-        return message.channel.send(
-            `${song.title} has been added to the queue!`
-        )
-    }
-}
-
-function skip(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        )
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could skip!")
-    serverQueue.connection.dispatcher.end()
-}
-
-function stop(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        )
-    serverQueue.songs = []
-    serverQueue.connection.dispatcher.end()
-}
-
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id)
-    if (!song) {
-        serverQueue.voiceChannel.leave()
-        queue.delete(guild.id)
-        return
-    }
-
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift()
-            play(guild, serverQueue.songs[0])
-        })
-        .on("error", (error) => console.error(error))
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`)
-}
 
 client.login(token)

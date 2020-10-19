d2e292bc519e4403310dc3cfcbaf0bb7b7118a46
Victor <victor@gorchilov.com>
2020-10-19 15:46:03 +0300

    [fix] calling play without arguments

--- commands/play.js
+++ commands/play.js
@@ -2,40 +2,38 @@ const ytdl = require('ytdl-core')
 const utils = require('../utils')
 
 module.exports = async function play(message) {
-    const args = message.content
-        .trim()
-        .split(/(?<=^\S+)\s/)[1]
-        .split(' ')
+    const args = message.content.trim().split(' ')
 
-    if (message.member.voice.channel) {
-        if (args.length == 0) {
-            message.channel.send(new utils.Message().setTitle('**No song specified**'))
-            return
-        }
-        let video = await utils.search(args)
-
-        console.log(video.url)
-        let output = new utils.Message()
-            .setTitle('**Playing Song**')
-            .setThumbnail(video.thumbnail)
-            .setDescription(`[${video.title}](${video.url})`)
-        message.channel.send(output)
-        message.member.voice.channel
-            .join()
-            .then((connection) => {
-                console.log(`joined channel in ${message.member.voice.channel.guild}`)
-                let yt = ytdl(video.url)
-                // .on("end", (end) => {
-                //   console.log("left channel");
-                //   message.member.voice.channel.leave();
-                // });
-                connection.play(yt, {
-                    quality: 'highestaudio',
-                    volume: 1,
-                })
-            })
-            .catch((err) => console.log(err))
-    } else {
+    if (!message.member.voice.channel) {
         message.channel.send(new utils.Message().setTitle('You need to be in a voice channel to play'))
+        return
     }
+
+    if (args.length == 1) {
+        message.channel.send(new utils.Message().setTitle('**No song specified**'))
+        return
+    }
+
+    let video = await utils.search(args)
+
+    console.log(video.url)
+    let output = new utils.Message()
+        .setTitle('**Playing Song**')
+        .setThumbnail(video.thumbnail)
+        .setDescription(`[${video.title}](${video.url})`)
+    message.channel.send(output)
+    message.member.voice.channel
+        .join()
+        .then((connection) => {
+            console.log(`joined channel in ${message.member.voice.channel.guild}`)
+            let yt = ytdl(video.url).on('end', (end) => {
+                console.log('left channel')
+                message.member.voice.channel.leave()
+            })
+            connection.play(yt, {
+                quality: 'highestaudio',
+                volume: 1,
+            })
+        })
+        .catch((err) => console.log(err))
 }
